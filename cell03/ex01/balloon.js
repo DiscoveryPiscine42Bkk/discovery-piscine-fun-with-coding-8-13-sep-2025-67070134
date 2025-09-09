adf13f1a411c8812f843a8b149206b30a0347688
ATOMPassakorn <67070134@kmitl.ac.th>
2025-09-09 21:34:50 +0700

    update balloon

--- cell03/ex01/balloon.js
+++ cell03/ex01/balloon.js
@@ -1,17 +1,34 @@
-  const balloon = document.getElementById('balloon');
+const balloon = document.getElementById('balloon');
   const colors = ['red', 'green', 'blue'];
   let colorIndex = 0;
   let size = 200;
 
-  balloon.onclick = () => {
+  function nextColor() {
+    colorIndex = (colorIndex + 1) % colors.length;
+    balloon.style.backgroundColor = colors[colorIndex];
+  }
+
+  function prevColor() {
+    colorIndex = (colorIndex - 1 + colors.length) % colors.length;
+    balloon.style.backgroundColor = colors[colorIndex];
+  }
+
+  balloon.addEventListener('click', () => {
     size += 10;
     if (size > 420) {
       size = 200;
       colorIndex = 0;
+      balloon.style.backgroundColor = colors[colorIndex];
     } else {
-      colorIndex = (colorIndex + 1) % colors.length;
+      nextColor();
     }
     balloon.style.width = size + 'px';
     balloon.style.height = size + 'px';
-    balloon.style.backgroundColor = colors[colorIndex];
-  };
+  });
+
+  balloon.addEventListener('mouseleave', () => {
+    size = Math.max(200, size - 5);
+    prevColor();
+    balloon.style.width = size + 'px';
+    balloon.style.height = size + 'px';
+  });
